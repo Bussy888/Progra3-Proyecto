5b749f8c1058509ffd53f2ef26b6c879c70e4cc7
Vivian Marino <marino6733784@gmail.com>
2022-12-01 09:09:07 -0400

    activities que usaremos

---
+++ app/src/main/java/com/grupoC/anderylosandersaurios/GameOverActivity.kt
@@ -0,0 +1,11 @@
+package com.grupoC.anderylosandersaurios
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameOverActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_over)
+    }
+}
