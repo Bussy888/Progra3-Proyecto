05991249074abd5216234bfe759d94b6cc3b03cf
Vivian Marino <marino6733784@gmail.com>
2022-12-07 19:07:23 -0400

    TESTING VOLUME BAR!!!!!!

--- app/src/main/java/com/grupoC/anderylosandersaurios/SettingsActivity.kt
+++ app/src/main/java/com/grupoC/anderylosandersaurios/SettingsActivity.kt
@@ -5,19 +5,46 @@ import android.os.Bundle
 import android.content.Intent
 import android.media.AudioManager
 import android.media.MediaPlayer
+import android.os.Build
+import android.widget.SeekBar
+import androidx.annotation.RequiresApi
 import androidx.core.content.getSystemService
 import com.grupoC.anderylosandersaurios.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySettingsBinding
     private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var audioManager: AudioManager
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        mediaPlayer= MediaPlayer.create(this, R.birds)
+        mediaPlayer= MediaPlayer.create(this, R.raw.nokia1994)
+        audioManager=getSystemService(AUDIO_SERVICE) as AudioManager
+
+        val max = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+        val currentVol = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+
+        binding.barVolume.max = max
+        binding.barVolume.progress = currentVol
+
+        binding.barVolume.setOnSeekBarChangeListener( object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // you can probably leave this empty
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // you can probably leave this empty
+            }
+
+        })
 
         setVolumeControlStream(AudioManager.STREAM_MUSIC)
         val audioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
