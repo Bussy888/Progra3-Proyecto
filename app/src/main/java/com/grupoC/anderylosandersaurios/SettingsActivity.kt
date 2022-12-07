339834e3c63a03a518d7ae4c121939c4e86f6bde
Vivian Marino <marino6733784@gmail.com>
2022-12-07 18:46:49 -0400

    PANTALLA DE OPTIONS/SETTINGS

--- app/src/main/java/com/grupoC/anderylosandersaurios/SettingsActivity.kt
+++ app/src/main/java/com/grupoC/anderylosandersaurios/SettingsActivity.kt
@@ -3,17 +3,30 @@ package com.grupoC.anderylosandersaurios
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
 import androidx.core.content.getSystemService
-import com.grupoC.anderylosandersaurios.databinding.ActivityMainBinding
+import com.grupoC.anderylosandersaurios.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
+    private lateinit var binding: ActivitySettingsBinding
+    private lateinit var mediaPlayer: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        val audioManager = binding.root.context.getSystemService(AUDIO_SERVICE)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        mediaPlayer= MediaPlayer.create(this, R.birds)
+
+        setVolumeControlStream(AudioManager.STREAM_MUSIC)
+        val audioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
+
+    }
+
+    private fun initControls(){
+
+
     }
 
 }
