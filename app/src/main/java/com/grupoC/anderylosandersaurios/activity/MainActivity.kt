39b2eff7311acec75d2a7c1dae3393afa5af7491
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2022-12-11 13:31:09 -0400

    popups, fonts, fixes in sound and activity.

--- app/src/main/java/com/grupoC/anderylosandersaurios/activity/MainActivity.kt
+++ app/src/main/java/com/grupoC/anderylosandersaurios/activity/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.grupoC.anderylosandersaurios.activity
 
 import android.animation.ObjectAnimator
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,12 +16,16 @@ import com.grupoC.anderylosandersaurios.R
 import com.grupoC.anderylosandersaurios.classes.Cabinet
 import com.grupoC.anderylosandersaurios.classes.MediatorGame
 import com.grupoC.anderylosandersaurios.databinding.ActivityMainBinding
+import com.grupoC.anderylosandersaurios.databinding.ItemPopupMenuBinding
+import com.grupoC.anderylosandersaurios.databinding.ItemPopupSettingsBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var thunderSound : MediaPlayer
     private lateinit var binding: ActivityMainBinding
     private lateinit var game: MediatorGame
+    private lateinit var bindingPopupMenu : ItemPopupMenuBinding
+    private lateinit var bindingPopupSettings : ItemPopupSettingsBinding
     var i = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,45 +34,68 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+            //Inicialización
+            game = MediatorGame(
+                Cabinet("blue", 0),
+                Cabinet("red", 0),
+                Cabinet("yellow", 0),
+                Cabinet("green", 0)
+            )
+            binding.redScore.text = "0"
+            binding.yellowScore.text = "0"
+            binding.blueScore.text = "0"
+            binding.greenScore.text = "0"
 
-        //Inicialización
-        game = MediatorGame(
-            Cabinet("blue", 0),
-            Cabinet("red", 0),
-            Cabinet("yellow", 0),
-            Cabinet("green", 0)
-        )
-        binding.redScore.text = "0"
-        binding.yellowScore.text = "0"
-        binding.blueScore.text = "0"
-        binding.greenScore.text = "0"
-
-        // El timer
-        binding.progressBar.max = 100
-        progressBarCycle()
-        timer()
-        thunderSound = MediaPlayer.create(this, R.raw.thunder)
+            binding.progressBarBackGround.visibility = View.GONE
+            binding.progressBar.visibility = View.GONE
+            binding.lightBulb.visibility = View.GONE
+            // El timer
+            timer()
+            thunderSound = MediaPlayer.create(this, R.raw.thunder)
 
 
+            // Los botones
+            binding.buttonOne.setOnClickListener {
+                binding.redScore.text = game.checking(1).toString()
+            }
 
-        // Los botones
-        binding.buttonOne.setOnClickListener {
-            binding.redScore.text = game.checking(1).toString()
-        }
+            binding.buttonTwo.setOnClickListener {
+                binding.yellowScore.text = game.checking(2).toString()
+            }
 
-        binding.buttonTwo.setOnClickListener {
-            binding.yellowScore.text = game.checking(2).toString()
-        }
+            binding.buttonThree.setOnClickListener {
+                binding.blueScore.text = game.checking(3).toString()
+            }
 
-        binding.buttonThree.setOnClickListener {
-            binding.blueScore.text = game.checking(3).toString()
-        }
-
-        binding.buttonFour.setOnClickListener {
-            binding.greenScore.text = game.checking(4).toString()
-        }
+            binding.buttonFour.setOnClickListener {
+                binding.greenScore.text = game.checking(4).toString()
+            }
+            binding.buttonMenu.setOnClickListener {
+                managePopupMenu()
+            }
+            binding.imageCoffeeCupS.setOnClickListener {
+                managePopupSettings()
+            }
 
     }
+
+    override fun onStop() {
+        super.onStop()
+        finishAll()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        finishAll()
+    }
+    fun hardMode(){
+        binding.progressBarBackGround.visibility = View.VISIBLE
+        binding.progressBar.visibility = View.VISIBLE
+        binding.lightBulb.visibility = View.VISIBLE
+        binding.progressBar.max = 100
+        progressBarCycle()
+    }
+
     fun timer(){
         object : CountDownTimer(300000,1000){
             override fun onTick(millisUntilFinished: Long) {
@@ -77,10 +107,12 @@ class MainActivity : AppCompatActivity() {
             override fun onFinish() {
                     val intent = Intent(applicationContext, GameOverActivity::class.java).apply {}
                     startActivity(intent)
+                    finishAll()
             }
         }.start()
 
     }
+
     fun progressBarCycle(){
         object  : CountDownTimer(20000, 1000){
             override fun onTick(millisUntilFinished: Long) {
@@ -93,6 +125,7 @@ class MainActivity : AppCompatActivity() {
                 i++;
                 thunder(3000, 1000, binding.backgroundWhite)
                 thunderSound.start()
+
                 binding.progressBar.progress = 0
                 i = 0
 
@@ -122,5 +155,41 @@ class MainActivity : AppCompatActivity() {
         }else{
             "${(millisUntilFinished/ 1000) % 60}"
         }
+    }
+    fun managePopupSettings() {
+        bindingPopupSettings = ItemPopupSettingsBinding.inflate(layoutInflater)
+        val dialog = Dialog(this)
+        dialog.setContentView(bindingPopupSettings.root)
+        dialog.setCancelable(true)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+        bindingPopupSettings.buttonAccept.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java).apply {}
+            startActivity(intent)
+            finishAll()
+        }
+        bindingPopupSettings.buttonCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+    }
+    fun managePopupMenu(){
+        bindingPopupMenu = ItemPopupMenuBinding.inflate(layoutInflater)
+        val dialog = Dialog(this)
+        dialog.setContentView(bindingPopupMenu.root)
+        dialog.setCancelable(true)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+        bindingPopupMenu.buttonAccept.setOnClickListener {
+            val intent = Intent(this, MainMenuActivity::class.java).apply {}
+            startActivity(intent)
+            finishAll()
+        }
+        bindingPopupMenu.buttonCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+    }
+    fun finishAll(){
+        finish()
+        thunderSound.stop()
     }
 }
