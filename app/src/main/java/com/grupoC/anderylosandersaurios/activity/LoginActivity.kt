d7aca1de88de844b43f082eee6ae262281d57ce6
Vivian Marino <marino6733784@gmail.com>
2022-12-12 08:40:23 -0400

    LOG IN

--- app/src/main/java/com/grupoC/anderylosandersaurios/activity/LoginActivity.kt
+++ app/src/main/java/com/grupoC/anderylosandersaurios/activity/LoginActivity.kt
@@ -11,11 +11,15 @@ import androidx.annotation.RequiresApi
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.grupoC.anderylosandersaurios.databinding.ActivityLoginBinding
 import com.grupoC.anderylosandersaurios.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+    var currentUser: FirebaseUser?=null
 
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,5 +48,73 @@ class LoginActivity : AppCompatActivity() {
             // appear for some time
             controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
+    }
+
+    private fun initUi(){
+        auth=FirebaseAuth.getInstance()
+        currentUser=auth.currentUser
+
+        binding.run{
+            buttonLogIn.setOnClickListener{
+                val email=editEmail.text.toString()
+                val password = editPassword.text.toString()
+                if(validateData(email, password)){
+                    loginUser(email, password)
+                }
+            }
+            buttonNewUser.setOnClickListener{
+                val email=editEmail.text.toString()
+                val password=editPassword.text.toString()
+                if(validateData(email, password)){
+                    createNewUser(email,password)
+                }
+            }
+        }
+    }
+
+    private fun validateData(email: String, password: String): Boolean{
+        var valid = true
+        if(email.isEmpty()){
+            valid = false
+
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            valid = false
+        }
+        return valid
+    }
+
+    private fun createNewUser(email: String, password: String){
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this)
+        { task ->
+            if(task.isSuccessful){
+                sendToast()
+            } else{
+                sendToast()
+            }
+        }
+
+    }
+
+    private fun loginUser(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password).
+        addOnCompleteListener(this) { task ->
+            if(task.isSuccessful){
+                redirectActivity()
+            } else{
+                sendToast()
+            }
+        }
+    }
+
+    // TODO: IR A MAIN MENU ACTIVITY
+    private fun redirectActivity(){
+        val intentRedirect = Intent(this, MainMenuActivity::class.java)
+        startActivity(intentRedirect)
+        finish()
+    }
+
+    // TODO: MANDAR TOAST
+    private fun sendToast(){
+
     }
 }
